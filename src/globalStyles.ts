e527e0a5de67fbd9b95e19b619bb372321261ca7
Davide Aresta <davide.aresta84@gmail.com>
2026-05-08 21:29:38 +0200

    fix: resolve conflict in src/globalStyles.ts

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,3 +1,4 @@
+```typescript
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
@@ -18,3 +19,4 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export default GlobalStyle;
+```
