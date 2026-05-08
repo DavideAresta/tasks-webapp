c082bb1eaa5a1d8b0976ff094e764f9ab8e191f4
Davide Aresta <davide.aresta84@gmail.com>
2026-05-08 21:29:50 +0200

    fix: resolve conflict in src/globalStyles.ts

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,4 +1,21 @@
+```typescript
 import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
 
 export const GlobalStyles = createGlobalStyle`
   body {
@@ -15,3 +32,6 @@ export const GlobalStyles = createGlobalStyle`
       monospace;
   }
 `;
+
+export default GlobalStyle;
+```
