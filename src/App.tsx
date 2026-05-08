20c824aa7b6805085e1306cda33ec0fcc7306d7e
Davide Aresta <davide.aresta84@gmail.com>
2026-05-08 21:28:17 +0200

    fix: resolve merge conflict in App.tsx (auto-merged with main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
 import TaskList from './components/TaskList';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={TaskList} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/" component={TaskList} />
+          {/* Add more routes here */}
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
